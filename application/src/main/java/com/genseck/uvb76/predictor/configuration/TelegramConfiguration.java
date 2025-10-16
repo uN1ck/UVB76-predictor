37cfa2052ac81118f59e2406ae7f6f48aed94913
genseck <qserxsdiv@gmail.com>
2025-10-16 18:44:09 +0300

    Init commit

---
+++ application/src/main/java/com/genseck/uvb76/predictor/configuration/TelegramConfiguration.java
@@ -0,0 +1,54 @@
+package com.genseck.uvb76.predictor.configuration;
+
+import com.genseck.uvb76.predictor.properties.TelegramProperties;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.GetChat;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@Import(TelegramProperties.class)
+public class TelegramConfiguration {
+
+    private final TelegramProperties properties;
+
+    @Bean
+    public TelegramBot newTelegramBot() {
+        TelegramBot bot = new TelegramBot(properties.getToken());
+
+        var response = bot.execute(new GetChat(properties.getChannelId()));
+        log.warn("Checking self-identity {}", response);
+
+        //
+//        var msgResponse = bot.execute(new SendMessage(properties.getChannelId(), "ID"));
+//        log.warn("ME {}", msgResponse);
+
+
+        bot.setUpdatesListener(updates -> {
+
+            updates.forEach(update -> {
+                log.warn("UPDATE: {}", update);
+            });
+
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }, e -> {
+            if (e.response() != null) {
+                // got bad response from telegram
+                e.response().errorCode();
+                e.response().description();
+            } else {
+                // probably network error
+                e.printStackTrace();
+            }
+        });
+
+        return bot;
+    }
+
+}
---
+++ application/src/main/java/com/genseck/uvb76/predictor/properties/TelegramProperties.java
@@ -0,0 +1,11 @@
+package com.genseck.uvb76.predictor.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("uvb76.telegram")
+public class TelegramProperties {
+    private String token;
+    private String channelId;
+}
